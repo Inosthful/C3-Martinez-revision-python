9109be289d919e49cd405e81f897d399a9df7096
Inosthful <inosthful@gmail.com>
2023-11-14 15:28:40 +0100

    exo 7 complet + fichier texte

--- exo/exo7.py
+++ exo/exo7.py
@@ -9,20 +9,15 @@ def countWords(test):
 
     except FileNotFoundError:
         print(f"Le fichier '{test}' n'a pas été trouvé.")
-        return None
-    except Exception as e:
-        print(f"Une erreur s'est produite : {e}")
-        return None
     
 def writeResult(fichierResultat, nbr):
-    try:
+    
         with open(fichierResultat, 'w', encoding='utf-8') as fichier_sortie:
             fichier_sortie.write(f"Nombre de mots dans le fichier : {nbr}")
 
         print(f"Résultat écrit dans le fichier '{fichierResultat}'.")
 
-    except Exception as e:
-        print(f"Une erreur s'est produite lors de l'écriture du résultat : {e}")
+
 
 
 test = 'text.txt'
