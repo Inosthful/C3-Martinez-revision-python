26aced215d3d8521cfaf7add988a0fbf7fc5f3f1
Inosthful <inosthful@gmail.com>
2023-11-14 14:43:42 +0100

    exo 6

---
+++ exo/exo6.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+# Obtenez la date et l'heure actuelles
+maintenant = datetime.now()
+
+# Affichez la date et l'heure au format YYYY-MM-DD HH:MM:SS
+
+print(f"Date et heure actuelles : {maintenant}")
