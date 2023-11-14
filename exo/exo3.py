4d11f78e4c09272539df1de5c14679abbfcee2fd
Inosthful <inosthful@gmail.com>
2023-11-14 15:33:28 +0100

    exo 3, rajout avec eval()

--- exo/exo3.py
+++ exo/exo3.py
@@ -1,27 +1,46 @@
+# def calculatrice():
+
+#     operation = input("Choisissez une opération (addition, multiplication, soustraction, division) : ")
+    
+#     if operation not in ['addition', 'multiplication', 'soustraction', 'division']:
+#         print("Opération non valide.")
+#         return
+    
+#     nombre1 = float(input("Entrez le premier nombre : "))
+#     nombre2 = float(input("Entrez le deuxième nombre : "))
+
+#     if operation == 'addition':
+#         resultat = nombre1 + nombre2
+#     elif operation == 'multiplication':
+#         resultat = nombre1 * nombre2
+#     elif operation == 'soustraction':
+#         resultat = nombre1 - nombre2
+#     elif operation == 'division':
+#         if nombre2 != 0:
+#             resultat = nombre1 / nombre2
+#         else:
+#             print("Division par zéro impossible.")
+#             return
+
+#     print(f"Résultat de l'opération {operation} : {resultat}")
+
+# calculatrice()
+
+
+# Avec eval ( donc n'importe quel expression mathématique ) 
+
 def calculatrice():
-
-    operation = input("Choisissez une opération (addition, multiplication, soustraction, division) : ")
+    expression = input("Entrez une expression mathématique : ")
     
-    if operation not in ['addition', 'multiplication', 'soustraction', 'division']:
-        print("Opération non valide.")
-        return
-    
-    nombre1 = float(input("Entrez le premier nombre : "))
-    nombre2 = float(input("Entrez le deuxième nombre : "))
+    try:
+        resultat = eval(expression)
+        print(f"Résultat de l'expression : {resultat}")
+        
+    except Exception as e:
+        print(f"Erreur lors de l'évaluation de l'expression : {e}")
 
-    if operation == 'addition':
-        resultat = nombre1 + nombre2
-    elif operation == 'multiplication':
-        resultat = nombre1 * nombre2
-    elif operation == 'soustraction':
-        resultat = nombre1 - nombre2
-    elif operation == 'division':
-        if nombre2 != 0:
-            resultat = nombre1 / nombre2
-        else:
-            print("Division par zéro impossible.")
-            return
 
-    print(f"Résultat de l'opération {operation} : {resultat}")
+
+
 
 calculatrice()
