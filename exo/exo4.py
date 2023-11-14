a38593fa637ccb0a867018d23b1bd0c5f9e5fcb1
Inosthful <inosthful@gmail.com>
2023-11-14 14:43:51 +0100

    exo 4

--- exo/exo4.py
+++ exo/exo4.py
@@ -2,12 +2,12 @@ import random
 
 liste_nombres = [random.randint(1, 100) for _ in range(10)]
 
-# Trouver le maximum, le minimum et calculer la moyenne
+
 maximum = max(liste_nombres)
 minimum = min(liste_nombres)
 moyenne = sum(liste_nombres) / len(liste_nombres)
 
-# Afficher les résultats
+
 print(f"Liste de nombres : {liste_nombres}")
 print(f"Maximum : {maximum}")
 print(f"Minimum : {minimum}")
