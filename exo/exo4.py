e1a8aa3e7efc8362e588205fc7278b2c6d7f5110
Inosthful <inosthful@gmail.com>
2023-11-14 16:44:15 +0100

    exo 4 sans max min et sum

--- exo/exo4.py
+++ exo/exo4.py
@@ -2,11 +2,23 @@ import random
 
 liste_nombres = [random.randint(1, 100) for _ in range(10)]
 
+maximum = liste_nombres[0]
+minimum = liste_nombres[0]
 
-maximum = max(liste_nombres)
-minimum = min(liste_nombres)
-moyenne = sum(liste_nombres) / len(liste_nombres)
+somme = 0
+compteur = 0
 
+# Parcourir la liste pour trouver le maximum, le minimum et calculer la somme
+for nombre in liste_nombres:
+    if nombre > maximum:
+        maximum = nombre
+    elif nombre < minimum:
+        minimum = nombre
+    somme += nombre
+    compteur += 1
+
+# Calculer la moyenne
+moyenne = somme / compteur
 
 print(f"Liste de nombres : {liste_nombres}")
 print(f"Maximum : {maximum}")
