49cd0a850d067aaaa5601df38005a9ff6e8e7d1e
Inosthful <inosthful@gmail.com>
2023-11-14 15:24:39 +0100

    exo 3 sous forme de fonction ( ( mieux )

--- exo/exo3.py
+++ exo/exo3.py
@@ -1,18 +1,27 @@
-nombre1 = float(input("Entrez le premier nombre : "))
-nombre2 = float(input("Entrez le deuxième nombre : "))
+def calculatrice():
 
+    operation = input("Choisissez une opération (addition, multiplication, soustraction, division) : ")
+    
+    if operation not in ['addition', 'multiplication', 'soustraction', 'division']:
+        print("Opération non valide.")
+        return
+    
+    nombre1 = float(input("Entrez le premier nombre : "))
+    nombre2 = float(input("Entrez le deuxième nombre : "))
 
-addition = nombre1 + nombre2
-soustraction = nombre1 - nombre2
-multiplication = nombre1 * nombre2
+    if operation == 'addition':
+        resultat = nombre1 + nombre2
+    elif operation == 'multiplication':
+        resultat = nombre1 * nombre2
+    elif operation == 'soustraction':
+        resultat = nombre1 - nombre2
+    elif operation == 'division':
+        if nombre2 != 0:
+            resultat = nombre1 / nombre2
+        else:
+            print("Division par zéro impossible.")
+            return
 
-if nombre2 != 0:
-    division = nombre1 / nombre2
-else:
-    division = "Division par zéro impossible"
+    print(f"Résultat de l'opération {operation} : {resultat}")
 
-
-print(f"Addition : {addition}")
-print(f"Soustraction : {soustraction}")
-print(f"Multiplication : {multiplication}")
-print(f"Division : {division}")
+calculatrice()
